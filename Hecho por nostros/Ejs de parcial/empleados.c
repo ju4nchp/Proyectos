e931b9165d5853c8ba658e2075b40971e0436190
ju4nchp <146787313+ju4nchp@users.noreply.github.com>
2023-10-04 16:53:27 -0300

    Update empleados.c

--- Hecho por nostros/Ejs de parcial/empleados.c
+++ Hecho por nostros/Ejs de parcial/empleados.c
@@ -1,51 +1,56 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-<CodeBlocks_project_file>
-	<FileVersion major="1" minor="6" />
-	<Project>
-		<Option title="parcial" />
-		<Option execution_dir="D:/proyecto/parcial/" />
-		<Option pch_mode="2" />
-		<Option compiler="gcc" />
-		<Build>
-			<Target title="Debug">
-				<Option output="bin/Debug/parcial" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Debug/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-g" />
-				</Compiler>
-			</Target>
-			<Target title="Release">
-				<Option output="bin/Release/parcial" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Release/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-O2" />
-				</Compiler>
-				<Linker>
-					<Add option="-s" />
-				</Linker>
-			</Target>
-		</Build>
-		<Compiler>
-			<Add option="-Wall" />
-		</Compiler>
-		<Unit filename="D:/proyecto/parcial/Untitled1.c">
-			<Option compilerVar="CC" />
-		</Unit>
-		<Unit filename="D:/proyecto/parcial/Untitled2.c">
-			<Option compilerVar="CC" />
-		</Unit>
-		<Unit filename="D:/proyecto/parcial/Untitled3.c">
-			<Option compilerVar="CC" />
-		</Unit>
-		<Unit filename="D:/proyecto/parcial/main.c">
-			<Option compilerVar="CC" />
-		</Unit>
-		<Extensions>
-			<lib_finder disable_auto="1" />
-		</Extensions>
-	</Project>
-</CodeBlocks_project_file>
+#include <stdio.h>
+
+#define NUM_EMPLEADOS 6
+
+int main() {
+    char nombre[100];
+    char sexo;
+    float horasTrabajadas, sueldoPorHora;
+    float sueldoTotalHombres = 0, sueldoTotalMujeres = 0;
+    int contadorHombres = 0, contadorMujeres = 0;
+
+    for (int i = 0; i < NUM_EMPLEADOS; i++) {
+        printf("Ingrese el nombre del empleado %d: ", i + 1);
+        scanf("%s", nombre);
+
+        printf("Ingrese el sexo del empleado (M/F): ");
+        scanf(" %c", &sexo);
+
+        printf("Ingrese las horas trabajadas por el empleado: ");
+        scanf("%f", &horasTrabajadas);
+
+        printf("Ingrese el sueldo por hora del empleado: ");
+        scanf("%f", &sueldoPorHora);
+
+        // Calcular el sueldo para este empleado
+        float sueldo = horasTrabajadas * sueldoPorHora;
+
+        // Mostrar el nombre y sueldo del empleado
+        printf("Nombre: %s, Sueldo: %.2f\n", nombre, sueldo);
+
+        // Acumular el sueldo total por género
+        if (sexo == 'M' || sexo == 'm') {
+            sueldoTotalHombres += sueldo;
+            contadorHombres++;
+        } else if (sexo == 'F' || sexo == 'f') {
+            sueldoTotalMujeres += sueldo;
+            contadorMujeres++;
+        } else {
+            printf("Sexo no válido para el empleado %s.\n", nombre);
+        }
+    }
+
+    // Calcular el sueldo promedio por género
+    float sueldoPromedioHombres = sueldoTotalHombres / contadorHombres;
+    float sueldoPromedioMujeres = sueldoTotalMujeres / contadorMujeres;
+
+    // Mostrar los totales y promedios
+    printf("\nTotales:\n");
+    printf("Sueldo total de hombres: %.2f\n", sueldoTotalHombres);
+    printf("Sueldo total de mujeres: %.2f\n", sueldoTotalMujeres);
+    printf("\nPromedios:\n");
+    printf("Sueldo promedio de hombres: %.2f\n", sueldoPromedioHombres);
+    printf("Sueldo promedio de mujeres: %.2f\n", sueldoPromedioMujeres);
+
+    return 0;
+}
